8e8a4b136a13a21a4222fc6c9584023c507595e0
Kazhykeldy <kazhyk666@gmail.com>
2024-11-07 23:42:16 +0500

    comment 2

--- app/src/main/java/com/example/homework/secondActivity.kt
+++ app/src/main/java/com/example/homework/secondActivity.kt
@@ -1,13 +1,10 @@
 package com.example.homework
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.secondpage)
+        setContentView(R.layout.activity_second_page)
     }
 }
