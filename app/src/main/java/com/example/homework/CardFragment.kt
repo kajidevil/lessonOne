962be2575d34b569f5b6687448de1a72f752feb1
Ayat Rakhishev <a.rakhishev@gmail.com>
2024-11-11 23:53:49 +0500

    Navigation fix

--- app/src/main/java/com/example/homework/CardFragment.kt
+++ app/src/main/java/com/example/homework/CardFragment.kt
@@ -1,6 +1,8 @@
 package com.example.homework
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,6 +20,38 @@ class CardFragment : Fragment() {
     ): View {
         viewBinding = FragmentCardBinding.inflate(layoutInflater, container, false)
         return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewBinding.editText1.addTextChangedListener(textWatcher)
+        viewBinding.editText2.addTextChangedListener(textWatcher)
+        viewBinding.editText3.addTextChangedListener(textWatcher)
+
+        viewBinding.editBox.setOnCheckedChangeListener { _, _ -> checkFields() }
+        viewBinding.submitButton.setOnClickListener {
+
+        }
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            checkFields()
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
+    }
+
+    private fun checkFields() {
+        val isText1Filled = viewBinding.editText1.text.toString().isNotEmpty()
+        val isText2Filled = viewBinding.editText2.text.toString().isNotEmpty()
+        val isText3Filled = viewBinding.editText3.text.toString().isNotEmpty()
+        val isCheckBoxChecked = viewBinding.editBox.isChecked
+
+        viewBinding.submitButton.isEnabled =
+            isText1Filled && isText2Filled && isText3Filled && isCheckBoxChecked
     }
 
 }
--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -2,63 +2,36 @@ package com.example.homework
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.homework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+        setUpNavigation()
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        setupActionBarWithNavController(navController)
+    private fun setUpNavigation() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val inflater = navHostFragment.navController.navInflater
+        val graph = inflater.inflate(R.navigation.main_navigation)
+
+        navController = navHostFragment.navController
+        navController.setGraph(graph, null)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
-
-
-    /*// TextWatcher для отслеживания изменения текста в EditText
-
-    // Добавляем TextWatcher для каждого поля EditText
-       /* viewBinding.editText1.addTextChangedListener(textWatcher)
-        viewBinding.editText2.addTextChangedListener(textWatcher)
-        viewBinding.editText3.addTextChangedListener(textWatcher)
-
-        // Добавляем слушатель для CheckBox
-        viewBinding.editBox.setOnCheckedChangeListener { _, _ -> checkFields() }
-
-        // Обработчик для кнопки
-        viewBinding.submitButton.setOnClickListener {
-            // Ваш код для отправки данных
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
-
-        }*/
-    private val textWatcher = object : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            checkFields()
-        }
-
-        override fun afterTextChanged(s: Editable?) {}
-    }
-
-    // Функция для проверки всех полей
-    private fun checkFields() {
-        val isText1Filled = viewBinding.editText1.text.toString().isNotEmpty()
-        val isText2Filled = viewBinding.editText2.text.toString().isNotEmpty()
-        val isText3Filled = viewBinding.editText3.text.toString().isNotEmpty()
-        val isCheckBoxChecked = viewBinding.editBox.isChecked
-
-        viewBinding.submitButton.isEnabled =
-            isText1Filled && isText2Filled && isText3Filled && isCheckBoxChecked
-    }*/
 }
