b9e61b4fcaa2a2afec8b90fc3bf1e6d6daf1644e
Ayat Rakhishev <ayat.rakhishev@docusketch.com>
2025-01-13 23:58:24 +0500

    little changes

--- app/src/main/java/com/example/homework/presentation/MainActivity.kt
+++ app/src/main/java/com/example/homework/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.homework
+package com.example.homework.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import com.example.homework.R
 import com.example.homework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/homework/presentation/SecondFragment.kt
+++ app/src/main/java/com/example/homework/presentation/SecondFragment.kt
@@ -1,11 +1,10 @@
-package com.example.homework
+package com.example.homework.presentation
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.homework.databinding.FragmentCardBinding
 import com.example.homework.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
