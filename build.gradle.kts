95cf0dcc9126e718567109eb5409b3fc97ec63eb
Ayat Rakhishev <ayat.rakhishev@docusketch.com>
2025-01-20 23:56:23 +0500

    DI dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,13 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Hilt Gradle Plugin classpath
+        classpath(libs.hilt.android.gradle.plugin)  // Use the latest Hilt version
+    }
+}
